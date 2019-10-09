7e5a706d94937efae2b9a7f279fa0f77121199ce
Adrian Corcoran <adrian.corcoran@shopify.com>
2019-10-09 21:17:08 +0100

    #3 Basic Types

--- index.ts
+++ index.ts
@@ -1,1 +1,21 @@
-console.log(`Hello World`);
+const isOpen: boolean = false;
+
+const myName: string = "Adrian";
+
+const myAGE: number = 38;
+
+const list: number[] = [0, 1, 2];
+
+const me: [string, number, boolean] = ["Adrian", 38, false];
+
+enum Job {
+  WebDev,
+  WebDesigner,
+  PM
+}
+
+const job: Job = Job.WebDev;
+
+const phone: any = "Pixel";
+
+const tablet: any = 3;
